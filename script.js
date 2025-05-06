8070a1a284aa768c563eefd44121632e1ed9acf0
jenny <jenniferbarbosa4439@gmail.com>
2025-05-06 16:25:37 -0300

    fiz a tarefa de usuario

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(Olá);
+/* crie duas variaveis identificadas por a e b
+a tem valor de 10
+b tem valor de 20
+
+inverta os valores das variáveis e mostre no console*/
+
+let a = 10;
+let b = 20;
+let c = a;
+
+   a = b;
+   b = c;
+
+console.log(a, b);
+
+
---
+++ usuario.js
@@ -0,0 +1,18 @@
+/*crie um programa que:
+
+solicite o nome e a profissão do usuário 
+
+exiba um alert() com a seguinte mensagem usando interpolação:
+    "Olá, nomeDoUsuário! Que interessante saber que você trabalha como profissaoDoUsuario."
+    */
+
+
+
+    
+let nomeDoUsuario = prompt("Qual seu nome?")
+let profissaoDoUsuario = prompt("Qual sua profissão?")
+
+    alert(`Olá, ${nomeDoUsuario}! Que interessante saber que você trabalha como ${profissaoDoUsuario}.`)
+
+    
+// alert n precisa de console.log
